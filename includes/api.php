7d841b6ee6bb3a711f497dfec9c12721065dd21f
David Copano <soporte4@xerintel.es>
2019-04-25 11:23:38 +0200

    incluso de doctrine

---
+++ includes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+include('lib/php/loadAll.php');
+
+//https://www.doctrine-project.org/projects/doctrine1/en/latest/manual/dql-doctrine-query-language.html
+
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ includes/lib/php/loadDoctrine.php
@@ -0,0 +1,47 @@
+<?php
+$bd_config = new stdClass;
+$bd_config->host = 'localhost';
+$bd_config->user = 'dueling_user';
+$bd_config->pass = 'H00Vhnf-H1(3';
+$bd_config->db = 'dueling_mysql';
+
+// OJO , revisar una vez puesto en produccion
+$pathToModels = dirname(__FILE__) . '/../models';
+$pathToDoctrine = dirname(__FILE__) . '/../vendor/doctrine/Doctrine.php';
+
+//Cargamos la libreria
+require_once $pathToDoctrine;
+spl_autoload_register(array('Doctrine', 'autoload'));
+spl_autoload_register(array('Doctrine', 'modelsAutoload'));
+
+//Creamos el manager
+$manager = Doctrine_Manager::getInstance();
+
+//Configuramos el manager
+$manager->setAttribute(Doctrine::ATTR_VALIDATE, Doctrine::VALIDATE_ALL);
+$manager->setAttribute(Doctrine::ATTR_EXPORT, Doctrine::EXPORT_ALL);
+$manager->setAttribute(Doctrine::ATTR_MODEL_LOADING, Doctrine::MODEL_LOADING_CONSERVATIVE);
+//$manager->setAttribute(Doctrine::ATTR_AUTO_ACCESSOR_OVERRIDE, TRUE);
+$manager->setAttribute(Doctrine::ATTR_AUTOLOAD_TABLE_CLASSES, TRUE);
+
+//Creamos la conexion a la db
+$conn = Doctrine_Manager::connection('mysql://'.$bd_config->user.':'.$bd_config->pass.'@'.$bd_config->host.'/'.$bd_config->db, 'doctrine');
+$conn->setCharset('utf8');
+
+//Condicion para generar los modelos
+//si se generan los modelos los header(location no funcionan)
+//se genera un modal de bootstrap
+if (isset($_REQUEST['generarModelos'])) {
+	$_msg=new stdClass();
+	$_msg->type='success';
+	$_msg->text='Modelos generados correctamente';
+
+    Doctrine_Core::generateModelsFromDb(
+            $pathToModels, array('doctrine'), array(
+        'generateTableClasses' => TRUE
+            )
+    );
+} else {
+    Doctrine_Core::loadModels($pathToModels);
+}
+?>
